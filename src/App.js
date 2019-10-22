49c7b80d4888f04e798c919719dab455cc6dba51
Damola Adewunmi <damolasd@gmail.com>
2019-10-22 11:31:54 +0100

    Sign in page componen done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SignIn from './components/SignIn';
+import styled from 'styled-components';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link} from 'react-router-dom';
+
+
+const NavBar = styled.nav` 
+  display: flex;
+  justify-content: flex-end;
+  text-decoration: none;
+  div {
+    
+    margin: 10px;
+    padding: 10px;
+
+    a {
+      text-decoration: none;
+    }
+  }
+
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <NavBar>
+            <div>
+              <Link to="/about">About</Link>
+            </div>
+            <div>
+              <Link to="/login">Login</Link>
+            </div>
+        </NavBar>
+        <Switch>
+          <Route path="/about">
+          </Route>
+          <Route path="/login">
+            <SignIn />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
